08ed68f44359b3e4157d20b0eb76321425c9bc26
piggsoft <piggsoft@163.com>
2017-05-24 14:29:58 +0800

    去掉图片，改用font

--- js/data.js
+++ js/data.js
@@ -5,8 +5,7 @@ var templateData = [
         "items": [
             {
                 "id": "toolsModal1",
-                "alt": "",
-                "img": "img/portfolio/cabin.png",
+                "icon": "fa-address-book-o",
                 "title": "JSON Formatter",
                 "desc": '<p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>',
                 "itemDetails": [
@@ -14,6 +13,160 @@ var templateData = [
                         "title": "Client",
                         "href": "http://startbootstrap.com",
                         "desc": "Start Bootstrap"
+                    },
+                    {
+                        "title": "Date",
+                        "href": "http://startbootstrap.com",
+                        "desc": "April 2014"
+                    },
+                    {
+                        "title": "Service",
+                        "href": "http://startbootstrap.com",
+                        "desc": "Web Development"
+                    }
+                ]
+            },
+            {
+                "id": "toolsModal2",
+                "icon": "fa-address-book-o",
+                "title": "JSON Formatter",
+                "desc": '<p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>',
+                "itemDetails": [
+                    {
+                        "title": "Client",
+                        "href": "http://startbootstrap.com",
+                        "desc": "Start Bootstrap"
+                    },
+                    {
+                        "title": "Date",
+                        "href": "http://startbootstrap.com",
+                        "desc": "April 2014"
+                    },
+                    {
+                        "title": "Service",
+                        "href": "http://startbootstrap.com",
+                        "desc": "Web Development"
+                    }
+                ]
+            },
+            {
+                "id": "toolsModal3",
+                "icon": "fa-address-book-o",
+                "title": "JSON Formatter",
+                "desc": '<p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>',
+                "itemDetails": [
+                    {
+                        "title": "Client",
+                        "href": "http://startbootstrap.com",
+                        "desc": "Start Bootstrap"
+                    },
+                    {
+                        "title": "Date",
+                        "href": "http://startbootstrap.com",
+                        "desc": "April 2014"
+                    },
+                    {
+                        "title": "Service",
+                        "href": "http://startbootstrap.com",
+                        "desc": "Web Development"
+                    }
+                ]
+            },
+            {
+                "id": "toolsModal4",
+                "icon": "fa-address-book-o",
+                "title": "JSON Formatter",
+                "desc": '<p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>',
+                "itemDetails": [
+                    {
+                        "title": "Client",
+                        "href": "http://startbootstrap.com",
+                        "desc": "Start Bootstrap"
+                    },
+                    {
+                        "title": "Date",
+                        "href": "http://startbootstrap.com",
+                        "desc": "April 2014"
+                    },
+                    {
+                        "title": "Service",
+                        "href": "http://startbootstrap.com",
+                        "desc": "Web Development"
+                    }
+                ]
+            },
+            {
+                "id": "toolsModal5",
+                "icon": "fa-address-book-o",
+                "title": "JSON Formatter",
+                "desc": '<p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>',
+                "itemDetails": [
+                    {
+                        "title": "Client",
+                        "href": "http://startbootstrap.com",
+                        "desc": "Start Bootstrap"
+                    },
+                    {
+                        "title": "Date",
+                        "href": "http://startbootstrap.com",
+                        "desc": "April 2014"
+                    },
+                    {
+                        "title": "Service",
+                        "href": "http://startbootstrap.com",
+                        "desc": "Web Development"
+                    }
+                ]
+            },
+            {
+                "id": "toolsModal6",
+                "icon": "fa-address-book-o",
+                "title": "JSON Formatter",
+                "desc": '<p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>',
+                "itemDetails": [
+                    {
+                        "title": "Client",
+                        "href": "http://startbootstrap.com",
+                        "desc": "Start Bootstrap"
+                    },
+                    {
+                        "title": "Date",
+                        "href": "http://startbootstrap.com",
+                        "desc": "April 2014"
+                    },
+                    {
+                        "title": "Service",
+                        "href": "http://startbootstrap.com",
+                        "desc": "Web Development"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "id": "document",
+        "name": "文档",
+        "items": [
+            {
+                "id": "documentModal1",
+                "icon": "fa-address-book-o",
+                "title": "JSON Formatter",
+                "desc": '<p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>',
+                "itemDetails": [
+                    {
+                        "title": "Client",
+                        "href": "http://startbootstrap.com",
+                        "desc": "Start Bootstrap"
+                    },
+                    {
+                        "title": "Date",
+                        "href": "http://startbootstrap.com",
+                        "desc": "April 2014"
+                    },
+                    {
+                        "title": "Service",
+                        "href": "http://startbootstrap.com",
+                        "desc": "Web Development"
                     }
                 ]
             }
